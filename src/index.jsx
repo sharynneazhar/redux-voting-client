0181c42225f1ed05d7e3b33921fdd15d6a3f18d2
Sharynne Azhar <sharynneazhar@gmail.com>
2016-05-13 10:54:12 -0500

    connect redux to react

--- src/index.jsx
+++ src/index.jsx
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import {Router, Route, hashHistory} from 'react-router';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
+import reducer from './reducer';
 import App from './components/App';
-import Voting from './components/Voting';
-import Results from './components/Results';
+import {VotingContainer} from './components/Voting';
+import {ResultsContainer} from './components/Results';
+
+const store = createStore(reducer);
+store.dispatch({
+    type: 'SET_STATE',
+    state: {
+        vote: {
+            pair: ['Sunshine', '28 Days Later'],
+            tally: {Sunshine: 2}
+        }
+    }
+});
 
 const routes = <Route component={App}>
     <Route path="/results" component={Results} />
@@ -11,6 +25,8 @@ const routes = <Route component={App}>
 </Route>;
 
 ReactDOM.render(
-    <Router history={hashHistory}>{routes}</Router>,
-        document.getElementById('app')
-    );
+    <Provider store={store}>
+        <Router history={hashHistory}>{routes}</Router>
+    </Provider>,
+    document.getElementById('app')
+);
